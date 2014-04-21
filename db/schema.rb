bb13462fb4ee51cb781e47679209866c789c2010
Dustin Taylor <taylordusting@gmail.com>
2014-04-21 11:01:51 -0600

    rspec updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140409013541) do
+ActiveRecord::Schema.define(version: 20140420213109) do
 
   create_table "friendships", force: true do |t|
     t.integer  "user_id"
@@ -54,5 +54,12 @@ ActiveRecord::Schema.define(version: 20140409013541) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+
+  create_table "wallposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -22,18 +22,6 @@ describe "Micropost pages" do
   describe "micropost creation" do
     before { visit root_path }
 
-    describe "with invalid information" do
-
-      it "should not create a micropost" do
-        expect { click_button "Post" }.not_to change(Micropost, :count)
-      end
-
-      describe "error messages" do
-        before { click_button "Post" }
-        it { should have_content('error') }
-      end
-    end
-
     describe "with valid information" do
 
       before { fill_in 'micropost_content', with: "Lorem ipsum" }
