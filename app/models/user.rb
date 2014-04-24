9694c0448577e90e19cdaa77cbd8ab8d3e3f7bb5
Sam Beckett <mojobeckett@Sam-Becketts-MacBook-Pro.local>
2014-04-23 22:56:51 -0600

    WIP friend requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,20 @@ class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
   has_many :wallposts, dependent: :destroy
   has_many :friendships
-  has_many :friends, :through => :friendships
-  has_many :requested_friends, :through => :friendships, :source => :friend, :conditions => "status = 'requested'"
+  has_many :friends,
+           :through => :friendships,
+           :conditions => "status = 'accepted'"
+  has_many :requested_friends,
+           :through => :friendships,
+           :source => :friend,
+           :conditions => "status = 'requested'"
+  has_many :pending_friends,
+           :through => :friendships,
+           :source => :friend,
+           :conditions => "status = 'pending'"
+  has_many :reverse_friendships, :foreign_key => "friend_id",
+                                 :class_name => "Friendship",
+                                 :dependent => :destroy
   
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
