67e2f47bfbb39bad618647cf46a8f021fff85e04
jenraders <jenraders@gmail.com>
2014-05-01 14:48:09 -0600

    commit

--- app/models/wallpost.rb
+++ app/models/wallpost.rb
@@ -3,4 +3,12 @@ class Wallpost < ActiveRecord::Base
 	default_scope -> { order('created_at DESC')}
 	validates :content, presence: true, length: { maximum: 140 }
 	validates :user_id, presence: true
+
+
+	def self.from_users_friended_by(user)
+    friend_user_ids = "SELECT friend_id FROM friendships
+                         WHERE friend_id = :user_id"
+    where("user_id IN (#{friend_user_ids}) OR user_id = :user_id",
+          user_id: user.id)
+  end
 end
